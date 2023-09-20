7728ba52a0974a08c9e96e601890f321f760bc0a
Ivan Murashka <imurashka.me@gmail.com>
2023-09-20 23:11:26 +0200

    Logger auto configurator

---
+++ Configurator/Targets/LoggerTargetConfig.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+namespace Appegy.UniLogger
+{
+    [Serializable]
+    public abstract class LoggerTargetConfig
+    {
+        [SerializeField]
+        private FormatOptions _formatter;
+
+        public virtual FormatOptions Formatter => _formatter;
+
+        protected LoggerTargetConfig(FormatOptions formatter)
+        {
+            _formatter = formatter;
+        }
+    }
+}
---
+++ Configurator/Targets/UnityTargetConfig.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using UnityEngine;
+
+namespace Appegy.UniLogger
+{
+    [Serializable]
+    public class UnityTargetConfig : LoggerTargetConfig
+    {
+        private const FormatOptions DefaultFormatterOptions = FormatOptions.Tags | FormatOptions.LogType;
+        private const FormatOptions EditorFormatterOptions = FormatOptions.RichText | FormatOptions.Tags;
+
+        [SerializeField]
+        private bool _overrideForEditor = true;
+        [SerializeField]
+        private FormatOptions _editorFormatter = EditorFormatterOptions;
+
+        public override FormatOptions Formatter => (Application.isEditor && _overrideForEditor) ? _editorFormatter : base.Formatter;
+
+        public UnityTargetConfig()
+            : base(DefaultFormatterOptions)
+        {
+        }
+    }
+}
