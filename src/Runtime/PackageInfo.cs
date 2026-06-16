a3e17bda23bee3d37bf3208282b7f85c4e7b5d88
Ivan Murashka <imurashka.me@gmail.com>
2026-06-16 19:06:40 +0200

    Release tooling: signed release + bump-version (bump to 1.0.2) (#19)

---
+++ src/Runtime/PackageInfo.cs
@@ -0,0 +1,12 @@
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("Appegy.UniLogger.Tests")]
+
+namespace Appegy.UniLogger
+{
+    public static class PackageInfo
+    {
+        public const string Name = "com.appegy.unilogger";
+        public const string Version = "1.0.2";
+    }
+}
