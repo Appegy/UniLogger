bf13ac19b0ae385ec6152f6d23e3354e7ceb8908
Ivan Murashka <imurashka.me@gmail.com>
2026-06-17 12:30:11 +0200

    ci: use shared upm-ci reusable workflows (#21)

--- src/Runtime/PackageInfo.cs
+++ src/Runtime/PackageInfo.cs
@@ -7,6 +7,6 @@ namespace Appegy.UniLogger
     public static class PackageInfo
     {
         public const string Name = "com.appegy.unilogger";
-        public const string Version = "1.0.2";
+        public const string Version = "1.1.0";
     }
 }
